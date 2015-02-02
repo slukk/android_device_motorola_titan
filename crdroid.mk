ffbcf1d76532047347cc384b6d2a1cbee8e19405
slukk <slukkica@gmail.com>
2015-02-02 19:04:01 +0100

    Add pINKman and crDroid vendor to build

--- crdroid.mk
+++ crdroid.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/motorola/titan/full_titan.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/crdroid/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := titan
-PRODUCT_NAME := cm_titan
+PRODUCT_NAME := crdroid_titan
